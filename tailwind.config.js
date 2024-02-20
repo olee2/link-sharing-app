b2aa8875f1761227da3b2ec25572469a6bd50255
olee2 <oleaeik@protonmail.com>
2024-02-20 20:51:40 +0100

    Setup pages, colors, and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      instrumentSans: ["Instrument Sans"]
+    },
+    colors: {
+      purple: "#633CFF",
+      purpleHover: "#BEADFF",
+      purpleLight: "#EFEBFF",
+      greyDark: "#333333",
+      grey: "#737373",
+      borders: "#D9D9D9",
+      greyLight: "#FAFAFA",
+      white: "#FFFFFF",
+      red: "#FF3939"
+    },
     extend: {}
   },
 
